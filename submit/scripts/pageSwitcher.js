3326328d159a9dbf8d414cf4818206e01478c798
Elena Lebedeva <oninbo@gmail.com>
2017-03-26 01:14:49 +0300

    added page switching with javascript

---
+++ submit/scripts/pageSwitcher.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Lena Lebedeva on 25.03.2017.
+ */
+function hidePage(page) {
+    page.style.visibility = "hidden";
+    page.style.display = "none";
+}
+
+function showPage(page) {
+    page.style.visibility = "visible";
+    page.style.display = "block";
+}
+
+var showNextPage = function () {
+    var parent = this.parentElement;
+    hidePage(parent);
+    var nextPage = document.getElementById(parseInt(parent.id) + 1);
+    showPage(nextPage);
+}
+
+var showPreviousPage = function () {
+    var parent = this.parentElement;
+    hidePage(parent);
+    var previousPage = document.getElementById(parseInt(parent.id) - 1);
+    showPage(previousPage);
+}
+
+window.onload = function() {
+    var nextButtons = document.getElementsByClassName("next");
+    var previousButtons = document.getElementsByClassName("previous");
+    for (var i = 0; i < nextButtons.length; i++) {
+        nextButtons[i].addEventListener("click", showNextPage);
+        previousButtons[i].addEventListener("click", showPreviousPage);
+    }
+}
