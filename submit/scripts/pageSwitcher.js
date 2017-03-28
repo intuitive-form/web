d374bf506b5f31d43761dd59de16f9f57141cb95
Elena Lebedeva <oninbo@gmail.com>
2017-03-29 00:56:03 +0300

    simplified compulsory fields checking

--- submit/scripts/pageSwitcher.js
+++ submit/scripts/pageSwitcher.js
@@ -1,37 +1,15 @@
 /**
  * Created by Lena Lebedeva on 25.03.2017.
  */
-function hidePage(page) {
-    page.style.visibility = "hidden";
-    page.style.display = "none";
-}
-
-function showPage(page) {
-    page.style.visibility = "visible";
-    page.style.display = "block";
-}
-
-function showNextPage() {
-    var parent = this.parentElement;
-    if (checkCompulsoryFields(parent)) {
-        hidePage(parent);
-        var nextPage = document.getElementById(parseInt(parent.id) + 1);
-        showPage(nextPage);
-    }
-}
-
-function showPreviousPage() {
-    var parent = this.parentElement;
-    hidePage(parent);
-    var previousPage = document.getElementById(parseInt(parent.id) - 1);
-    showPage(previousPage);
-}
-
-window.onload = function() {
-    var nextButtons = document.getElementsByClassName("next");
-    var previousButtons = document.getElementsByClassName("previous");
-    for (var i = 0; i < nextButtons.length; i++) {
-        nextButtons[i].addEventListener("click", showNextPage);
-        previousButtons[i].addEventListener("click", showPreviousPage);
-    }
-}
+$( document ).ready(function() {
+    $( ".next" ).click(function() {
+        if (checkCompulsoryFields($(this).parent())){
+            $(this).parent().hide();
+            $("#"+(parseInt($(this).parent().attr('id')) + 1)).show();
+        }
+    });
+    $( ".previous" ).click(function() {
+        $(this).parent().hide();
+        $("#"+(parseInt($(this).parent().attr('id')) - 1)).show();
+    });
+});
