e43b188a2a5b1072319598949bdc2f774d11afa1
Elena Lebedeva <oninbo@gmail.com>
2017-03-28 23:37:27 +0300

    added some design

--- submit/scripts/compulsoryFieldsChecker.js
+++ submit/scripts/compulsoryFieldsChecker.js
@@ -2,12 +2,20 @@
  * Created by Lena Lebedeva on 28.03.2017.
  */
 function checkCompulsoryFields(page) {
-    var children = page.children;
+    var children = $(page).find(".compulsoryField");
+    var condition = true;
     for (var i = 0; i < children.length; i++){
-        if (children[i].className == "compulsoryField" && children[i].value == ""){
-            alert("All compulsory fields should be filled in.");
-            return false;
+        if (children[i].value == ""){
+            $(children[i]).parent().addClass("has-error");
+            if ($(children[i]).parent().find($(".alert")).length == 0) {
+                $(children[i]).parent().append("<div class='alert alert-danger'>All compulsory fields should be filled</div>");
+            }
+            condition = false;
+        }
+        else {
+            $(children[i]).parent().removeClass("has-error");
+            $(children[i]).parent().find($(".alert")).remove();
         }
     }
-    return true;
+    return condition;
 }
