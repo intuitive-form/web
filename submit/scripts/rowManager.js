728b9b0fbd253277fac29c52d2b0dcf8a1239c5f
Elena Lebedeva <oninbo@gmail.com>
2017-03-29 11:21:53 +0300

    added adding and deleting rows in tables

---
+++ submit/scripts/rowManager.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Lena Lebedeva on 29.03.2017.
+ */
+function recountNumbers(row) {
+    var numbers = $(row).parent().find(".number");
+    for (var i = 0; i < numbers.length; i++){
+        numbers[i].innerHTML = i+1;
+    }
+}
+function addRow() {
+    var row = $(this).parents("tr")[0];
+    if ($(row).parent().children().length == 1){
+        $(this).parent().find(".delete").show();
+    }
+    var newRow = $(row).clone();
+    $(newRow).find("input").val("")
+    $(newRow).find(".add").click(this, addRow);
+    $(newRow).find(".delete").click(this, deleteRow);
+    $(row).after(newRow);
+    recountNumbers(newRow);
+}
+function deleteRow() {
+    var row = $(this).parents("tr")[0];
+    var sibling = $(row).siblings()[0];
+    $(row).remove();
+    recountNumbers(sibling);
+    if ($(sibling).parent().children().length == 1){
+        $(sibling).parent().find(".delete").hide();
+    }
+}
+$( document ).ready(function() {
+    $( ".add" ).click(this, addRow);
+    $( ".delete" ).click(this, deleteRow);
+});
