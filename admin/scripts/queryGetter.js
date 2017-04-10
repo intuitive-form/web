d88713944df2c2c0217c0388dbb149911e239bbf
Elena Lebedeva <oninbo@gmail.com>
2017-04-11 01:42:33 +0300

    added query2

--- admin/scripts/queryGetter.js
+++ admin/scripts/queryGetter.js
@@ -6,26 +6,47 @@ function getInputValue(name) {
     return $("[name='" + name + "']").val();
 }
 
+function showData(data, form) {
+    var well = form.find(".well");
+    well.hide();
+    data = data.split("\n").join("<br />");
+    well.html(data);
+    well.fadeIn();
+}
+
 $(document).ready(function () {
     $(".get").click(function () {
         var form = $(this).parents("form");
-        var well = form.find(".well");
-        var queryName = form.attr("class").split("query")[1];
-        var requestName;
-        switch (queryName) {
-            case "1":
-                requestName = "/pubs/" + getInputValue("query1-year");
-                break;
-            case "2":
-            case "3":
-                requestName = "/courses/" + getInputValue("query3-laboratory-name") + "/" + getInputValue("query3-initial-date") + "/" + getInputValue("query3-final-date");
-                break;
+        if (checkData(form)) {
+            var queryName = form.attr("class").split("query")[1];
+            var requestName;
+            switch (queryName) {
+                case "1":
+                    requestName = "/pubs/" + getInputValue("query1-year");
+                    $.get(requestName, function (data) {
+                        showData(data, form);
+                    });
+                    break;
+                case "2":
+                    requestName = "/head/" + getInputValue("query2-unit-name");
+                    var text = "";
+                    $.get(requestName, function (data) {
+                        text += data + "\n";
+                        showData(text);
+                    });
+                    requestName = "/courses/" + getInputValue("query2-unit-name")
+                    $.get(requestName, function (data) {
+                        text += data + "\n";
+                        showData(text);
+                    });
+                    break;
+                case "3":
+                    requestName = "/courses/" + getInputValue("query3-laboratory-name") + "/" + getInputValue("query3-initial-date") + "/" + getInputValue("query3-final-date");
+                    $.get(requestName, function (data) {
+                        showData(data, form);
+                    });
+                    break;
+            }
         }
-        $.get(requestName, function (data) {
-            well.hide();
-            data = data.split("\n").join("<br />");
-            well.html(data);
-            well.fadeIn();
-        });
     });
 });
