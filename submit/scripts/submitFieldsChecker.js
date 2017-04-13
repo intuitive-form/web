0033ab35ee548ab664a10cd24931762b781bc60d
Elena Lebedeva <oninbo@gmail.com>
2017-04-13 20:07:27 +0300

    added submit alert

--- submit/scripts/submitFieldsChecker.js
+++ submit/scripts/submitFieldsChecker.js
@@ -10,12 +10,22 @@ function checkData(form) {
 $( document ).ready(function(){
     $("form").submit(function () {
         event.preventDefault();
-        var compulsoryFields = checkCompulsoryFields($(this));
-        var connectedFields = checkConnectedFields($(this));
-        var dates = dateCheck($(this));
-        $.post( "submit", {"value" : JSON.stringify(collectData())})
-            .done(function( data ) {
-                console.log( "Data Loaded: " + data );
-        });
+        if (checkData($(this))) {
+            $.post("submit", {"value": JSON.stringify(collectData())})
+                .done(function (data) {
+                    switch (data.status){
+                        case "ok":
+                            $(this).append("<div class='alert alert-success'><strong>Success!</strong> Form is successfully submitted.</div>");
+                            break;
+                        case "error":
+                            switch (data.reason){
+                                case "unit exists":
+                                    $(this).append("<div class='alert alert-danger'><strong>Error!</strong> Unit already exists.</div>");
+                                    break;
+                            }
+                            break;
+                    }
+                });
+        }
     });
 });
