828eb5c99968fe56915e1151ebcc947f8c18cc6e
Nikita Vilunov <nikitaoryol@gmail.com>
2017-04-13 20:38:30 +0300

    Submitter changes

--- submit/scripts/submitFieldsChecker.js
+++ submit/scripts/submitFieldsChecker.js
@@ -11,21 +11,29 @@ $( document ).ready(function(){
     $("form").submit(function () {
         event.preventDefault();
         if (checkData($(this))) {
-            $.post("submit", {"value": JSON.stringify(collectData())})
-                .done(function (data) {
-                    switch (data.status){
-                        case "ok":
-                            $(this).append("<div class='alert alert-success'><strong>Success!</strong> Form is successfully submitted.</div>");
-                            break;
-                        case "error":
-                            switch (data.reason){
-                                case "unit exists":
-                                    $(this).append("<div class='alert alert-danger'><strong>Error!</strong> Unit already exists.</div>");
-                                    break;
-                            }
-                            break;
-                    }
-                });
+            $.post("submit", { "value": JSON.stringify(collectData()) }).done(sendHandler);
         }
     });
 });
+
+function sendHandler(data) {
+    var reply;
+    try {
+        reply = JSON.parse(data);
+    } catch (e) {
+        //server error
+        return;
+    }
+    switch (reply.status) {
+        case "ok":
+            $(this).append("<div class='alert alert-success'><strong>Success!</strong> Form is successfully submitted.</div>");
+            break;
+        case "error":
+            switch (reply.reason) {
+                case "unit exists":
+                    $(this).append("<div class='alert alert-danger'><strong>Error!</strong> Unit already exists.</div>");
+                    break;
+            }
+            break;
+        }
+}
