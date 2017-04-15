43555018de26d08f959a3ead42a7a25c0bb2436f
Elena Lebedeva <oninbo@gmail.com>
2017-04-15 19:49:07 +0300

    minor fixes in submit; improved testing

--- scripts/testing.js
+++ scripts/testing.js
@@ -21,7 +21,7 @@ function fill(){
                         date = new Date(2016, 11, 31);
                     }
                     else {
-                        date = new Date(2016, Math.floor(Math.random() * 12), Math.floor(Math.random() * 30)+1);
+                        date = new Date(2016, Math.floor(Math.random() * 12), Math.floor(Math.random() * 30));
                     }
                     input.val(date.toISOString().slice(0, 10));
                 }
@@ -102,7 +102,7 @@ function test(data) {
     for (var i = 0; i < data.section3.projects.length; i++){
         if (i > 0) { newRow($("#projects")); }
         setInputValue("research-projects-title-" + (i + 1), data.section3.projects[i].title);
-        for (var j = 0; j < data.section3.projects.personnel.length; j++){
+        for (var j = 0; j < data.section3.projects[i].personnel.length; j++){
             if (i > 0) { newRow($("#personnel")); }
             setInputValue("research-projects-personnel-involved-name-" + (j + 1) + "-" + (i + 1), data.section3.projects[i].personnel[j].name);
         }
@@ -134,10 +134,51 @@ function test(data) {
         setInputValue("conference-publications-date-" + (i + 1), data.section3.conference_publications[i].date);
     }
     for (var i = 0; i < data.section3.journal_publications.length; i++){
+        if (i > 0) { newRow($("#journal-publications")); }
         setInputValue("journal-publications-title-" + (i + 1), data.section3.journal_publications[i].title);
         for (var j = 0; j < data.section3.journal_publications[i].authors.length; j++){
+            if (i > 0) { newRow($("#journal-authors")); }
             setInputValue("journal-publications-author-name-" + (j + 1) + "-" + (i + 1), data.section3.journal_publications[i].authors[j]);
         }
         setInputValue("journal-publications-date-" + (i + 1), data.section3.journal_publications[i].date);
     }
+    for (var i = 0; i < data.section4.patents.length; i++){
+        if (i > 0) { newRow($("#patents")); }
+        setInputValue("patents-title-" + (i + 1), data.section4.patents[i].title);
+        setInputValue("patents-country-" + (i + 1), data.section4.patents[i].country);
+    }
+    for (var i = 0; i < data.section4.licensing.length; i++){
+        if (i > 0) { newRow($("#licensing")); }
+        setInputValue("licensing-title-" + (i + 1), data.section4.patents[i].title);
+    }
+    for (var i = 0; i < data.section5.paper_awards.length; i++){
+        if (i > 0) { newRow($("#paper-awards")); }
+        setInputValue("paper-awards-title-" + (i + 1), data.section5.paper_awards[i].title);
+        for (var j = 0; j < data.section5.paper_awards[i].authors.length; j++){
+            if (i > 0) { newRow($("#paper-authors")); }
+            setInputValue("paper-awards-author-" + (j + 1) + "-" + (i + 1), data.section5.paper_awards[i].authors[j]);
+        }
+        setInputValue("paper-awards-awarding-" + (i + 1), data.section5.paper_awards[i].conference_journal);
+        setInputValue("paper-awards-wording-" + (i + 1), data.section5.paper_awards[i].wording);
+        setInputValue("paper-awards-date-" + (i + 1), data.section5.paper_awards[i].date );
+    }
+    for (var i = 0; i < data.section5.memberships.length; i++){
+        if (i > 0) { newRow($("#memberships")); }
+        setInputValue("memberships-name-" + (i + 1), data.section5.memberships[i].name);
+        setInputValue("memberships-organization-" + (i + 1), data.section5.memberships[i].organization);
+        setInputValue("memberships-date-" + (i + 1), data.section5.memberships[i].date);
+    }
+    for (var i = 0; i < data.section5.prizes.length; i++){
+        if (i > 0) { newRow($("#prizes")); }
+        setInputValue("prizes-recipient-" + (i + 1), data.section5.prizes[i].recipient);
+        setInputValue("prizes-name-" + (i + 1), data.section5.prizes[i].name);
+        setInputValue("prizes-granting-institution-" + (i + 1), data.section5.prizes[i].institution);
+        setInputValue("prizes-date-" + (i + 1), data.section5.prizes[i].date);
+    }
+    for (var i = 0; i < data.section6.collaborations.length; i++) {
+        if (i > 0) { newRow($("#industry-collaborations")); }
+        getInputValue("industry-collaborations-company-" + (i + 1), data.section6.collaborations[i].company);
+        getInputValue("industry-collaborations-nature-of-collaboration-" + (i + 1), data.section6.collaborations[i].nature);
+    }
+    setInputValue("other-information", data.section7.other);
 }
