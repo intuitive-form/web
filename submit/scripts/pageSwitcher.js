e5ce6508f0574bdce47fc42c9bf846a4744f68da
Elena Lebedeva <oninbo@gmail.com>
2017-04-22 15:38:35 +0300

    added scrolling to top

--- submit/scripts/pageSwitcher.js
+++ submit/scripts/pageSwitcher.js
@@ -4,11 +4,16 @@
 $( document ).ready(function() {
     $( ".next" ).click(function() {
         if (checkData($(this).parent())){
+            $("body").scrollTop(0);
             $(this).parent().hide();
             $("#"+(parseInt($(this).parent().attr('id')) + 1)).show();
         }
+        else {
+            $("body").scrollTop($(".alert:first").position().top/2)
+        }
     });
     $( ".previous" ).click(function() {
+        $("body").scrollTop(0)
         $(this).parent().hide();
         $("#"+(parseInt($(this).parent().attr('id')) - 1)).show();
     });
